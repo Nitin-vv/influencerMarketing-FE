c281c77f75be22b79241e67963e941ee2f111c96
Nitin <96164909+Nitin-vv@users.noreply.github.com>
2023-07-07 00:51:43 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
     name: 'localhost',
-    // apiUrl: 'http://localhost:3000',
     apiUrl: 'http://92.204.132.147:96',
     requestTimeout: 15000,
 };
